29e0537d74b59d5d0bc9cca659f145412abb0211
VSPrismo <112033235+VSPrismo@users.noreply.github.com>
2023-06-02 18:43:03 -0300

    commit inicial

---
+++ main.ts
@@ -0,0 +1,14 @@
+function multiplicacao(a: number, b: number): number {
+  return a * b;
+}
+
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+const resultado = multiplicacao(5, 3);
+console.log(resultado); 
+
+const saudacaoFinal = saudacao("João");
+console.log(saudacaoFinal); 
+
